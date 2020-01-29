7e26a64aa95870950d5d499cf4bbaebb16b56543
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-29 17:54:02 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,10 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year
+  FROM books
+  JOIN series 
+  ON books.series_id = series.id 
+  GROUP BY books.title
+  ORDER BY books.year ASC LIMIT 3;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -8,15 +13,32 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "FROM characters
+  GROUP BY (species)
+  ORDER BY species DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name 
+  FROM authors 
+  JOIN series 
+  ON series.author_id = authors.id 
+  JOIN subgenres 
+  ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title 
+  FROM series 
+  JOIN characters 
+  ON characters.id = character_books.character_id 
+  JOIN character_books
+  ON books.id = chracter_books.book_id
+  JOIN books
+  ON series.id = books.series_id
+  GROUP BY series.title
+  ORDER BY COUNT(*) DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
